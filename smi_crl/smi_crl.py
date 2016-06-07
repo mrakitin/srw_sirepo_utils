541a59516b97a68f778d2e1893b4b02a144ab0b5
Maksim Rakitin <maxim.rakitin@gmail.com>
2016-06-07 18:44:02 +0000

    CRL calculator updated - make usage of NumPy optional, by default it's not used.

--- smi_crl/smi_crl.py
+++ smi_crl/smi_crl.py
@@ -1,172 +1,80 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import division
 
-import numpy as np
+import copy
+import math
 
+try:
+    import numpy as np
+
+    NUMPY = True
+except:
+    NUMPY = False
+
+NUMPY = False  # explicitly avoid usage of NumPy.
 _DL_lens = 2e-3  # distance between two lenses within a cartridge [m].
 _DL_cart = 30e-3  # distance between centers of two neighbouring cartridges.
-_R_array = np.array([50, 200, 500]) * 1e-6  # radii of available lenses in different cartridges [m].
+_R_array = [50, 200, 500]  # radii of available lenses in different cartridges [um].
+_LENS_array = [1, 2, 4, 8, 16]  # possible number of lenses in cartridges.
 _P0 = 6.2  # distance from z=50.9 m to the first lens in the most upstream cartridge at the most upstream position of the transfocator.
 _ENERGY = 24000.0  # eV
-_LENS_CONFIG = {
-    'T_1_50': {
-        'radius': 50e-6,
-        'lens_number': 1,
-    },
-    'T_2_50': {
-        'radius': 50e-6,
-        'lens_number': 2,
-    },
-    'T_4_50': {
-        'radius': 50e-6,
-        'lens_number': 4,
-    },
-    'T_8_50': {
-        'radius': 50e-6,
-        'lens_number': 8,
-    },
-    'T_16_50': {
-        'radius': 50e-6,
-        'lens_number': 16,
-    },
-    'T_1_200': {
-        'radius': 200e-6,
-        'lens_number': 1,
-    },
-    'T_2_200': {
-        'radius': 200e-6,
-        'lens_number': 2,
-    },
-    'T_4_200': {
-        'radius': 200e-6,
-        'lens_number': 4,
-    },
-    'T_8_200': {
-        'radius': 200e-6,
-        'lens_number': 8,
-    },
-    'T_16_200': {
-        'radius': 200e-6,
-        'lens_number': 16,
-    },
-    'T_1_500': {
-        'radius': 500e-6,
-        'lens_number': 1,
-    },
-    'T_2_500': {
-        'radius': 500e-6,
-        'lens_number': 2,
-    },
-    'T_4_500': {
-        'radius': 500e-6,
-        'lens_number': 4,
-    },
-    'T_8_500': {
-        'radius': 500e-6,
-        'lens_number': 8,
-    },
-    'T_16_500': {
-        'radius': 500e-6,
-        'lens_number': 16,
-    }
-}
+_TETA0 = 60e-6  # rad
+
+_LENS_CONFIG = {}
+for i in _R_array:
+    for j in _LENS_array:
+        _LENS_CONFIG['T_{}_{}'.format(j, i)] = {
+            'radius': i * 1e-6,
+            'lens_number': j,
+        }
+
 _TRANSFOCATOR_CONFIG = [
     {
         'id': 1,
         'name': 'T_1_500',
-        'distance': 2 * _DL_cart,
-        'coordinate': 2 * _DL_cart,
+        'offset_cart': 2,
     },
     {
         'id': 2,
         'name': 'T_2_50',
-        'distance': _DL_cart - 1 * _DL_lens,
-        'coordinate': 3 * _DL_cart,
+        'offset_cart': 3,
     },
     {
         'id': 3,
         'name': 'T_8_500',
-        'distance': _DL_cart - 2 * _DL_lens,
-        'coordinate': 4 * _DL_cart,
+        'offset_cart': 4,
     },
     {
         'id': 4,
         'name': 'T_4_50',
-        'distance': _DL_cart - 8 * _DL_lens,
-        'coordinate': 5 * _DL_cart,
+        'offset_cart': 5,
     },
     {
         'id': 5,
         'name': 'T_1_200',
-        'distance': _DL_cart - 4 * _DL_lens,
-        'coordinate': 6 * _DL_cart,
+        'offset_cart': 6,
     },
     {
         'id': 6,
         'name': 'T_16_50',
-        'distance': _DL_cart - 1 * _DL_lens,
-        'coordinate': 7 * _DL_cart,
+        'offset_cart': 7,
     },
     {
         'id': 7,
         'name': 'T_8_50',
-        'distance': 2 * _DL_cart - 16 * _DL_lens,
-        'coordinate': 9 * _DL_cart,
+        'offset_cart': 9,
     },
     {
         'id': 8,
         'name': 'T_1_50',
-        'distance': 2 * _DL_cart - 8 * _DL_lens,
-        'coordinate': 11 * _DL_cart,
+        'offset_cart': 11,
     },
 ]
 
 
-def calc_distance(id1, id2):
-    for i in range(len(_TRANSFOCATOR_CONFIG)):
-        if id1 == _TRANSFOCATOR_CONFIG[i]['id']:
-            coord1 = _TRANSFOCATOR_CONFIG[i]['coordinate']
-            lens_number1 = _LENS_CONFIG[_TRANSFOCATOR_CONFIG[i]['name']]['lens_number']
-        if id2 == _TRANSFOCATOR_CONFIG[i]['id']:
-            coord2 = _TRANSFOCATOR_CONFIG[i]['coordinate']
-    return coord2 - coord1 - lens_number1 * _DL_lens
-
-
-def calc_delta(energy=_ENERGY, data_file='Be_delta.dat'):
-    data = np.loadtxt(data_file, skiprows=2)
-    try:
-        idx_next = np.where(data[:, 0] >= energy)[0][0]
-        idx_previous = np.where(data[:, 0] <= energy)[0][-1]
-    except IndexError:
-        raise Exception('Error! Use energy range from {} to {} eV.'.format(data[0, 0], data[-1, 0]))
-
-    if abs(data[idx_previous, 0] - energy) <= abs(data[idx_next, 0] - energy):
-        idx = idx_previous
-    else:
-        idx = idx_next
-    return {
-        'energy': data[idx, 0],
-        'delta': data[idx, 1]
-    }
-
-
-def calc_fs(R, delta):
-    return R / (2 * delta)
-
-
-def calc_T_dl(dl):
-    T_dl = np.array([
-        [1, dl],
-        [0, 1],
-    ])
-    return T_dl
-
-
-def calc_T_fs(fs):
-    T_fs = np.array([
-        [1, 0],
-        [-1 / fs, 1],
-    ])
-    return T_fs
+def calc_ideal_lens(R, N, delta):
+    return R / (2 * N * delta)
 
 
 def calc_lens_array(R, N):
@@ -176,17 +84,17 @@ def calc_lens_array(R, N):
     :param N: number of lenses in one cartridge.
     :return T_fs_accum: accumulated T_fs.
     """
-    T_dl = calc_T_dl(_DL_lens)
-    T_fs = calc_T_fs(calc_fs(R, calc_delta()['delta']))
+    T_dl = _calc_T_dl(_DL_lens)
+    T_fs = _calc_T_fs(_calc_fs(R, _calc_delta()['delta']))
 
-    T_fs_accum = np.dot(np.linalg.matrix_power(np.dot(T_fs, T_dl), N - 1), T_fs)
+    T_fs_accum = dot(matrix_power(dot(T_fs, T_dl), N - 1), T_fs)
     return T_fs_accum
 
 
 def calc_T_total(cart_ids):
     dist_list = []
     for i in range(len(cart_ids) - 1):
-        dist_list.append(calc_distance(cart_ids[i], cart_ids[i + 1]))
+        dist_list.append(_calc_distance(cart_ids[i], cart_ids[i + 1]))
 
     R_list = []
     N_list = []
@@ -199,16 +107,16 @@ def calc_T_total(cart_ids):
     if len(cart_ids) == 1:
         T = calc_lens_array(R_list[0], N_list[0])
     elif len(cart_ids) > 1:
-        A = calc_T_dl(dist_list[0])
+        A = _calc_T_dl(dist_list[0])
         B = calc_lens_array(R_list[0], N_list[0])
-        T = np.dot(A, B)
+        T = dot(A, B)
         for i in range(len(cart_ids) + len(cart_ids) - 3):
             if i % 2 == 0:
                 B = calc_lens_array(R_list[int((i + 2) / 2)], N_list[int((i + 2) / 2)])
-                T = np.dot(B, T)
+                T = dot(B, T)
             else:
-                A = calc_T_dl(dist_list[int((i + 1) / 2)])
-                T = np.dot(A, T)
+                A = _calc_T_dl(dist_list[int((i + 1) / 2)])
+                T = dot(A, T)
     else:
         raise Exception('No lenses in the beam!')
 
@@ -216,23 +124,150 @@ def calc_T_total(cart_ids):
 
 
 def calc_y_teta(T, y0, teta0):
-    return np.dot(T, np.array([y0, teta0]))
+    return dot(T, [y0, teta0])
 
 
-def calc_ideal_lens(R, N, delta):
-    return R / (2 * N * delta)
+def dot(A, B, use_numpy=NUMPY):
+    """Multiplies matrix A by matrix B."""
+    if use_numpy:
+        C = np.dot(A, B)
+    else:
+        B0 = B[0]
+        lenB = len(B)
+        lenA = len(A)
+        if len(A[0]) != lenB:  # Check matrix dimensions
+            raise Exception('Matrices have wrong dimensions')
+        if isinstance(B0, list) or isinstance(B0, tuple):  # B is matrix
+            lenB0 = len(B0)
+            C = [[0 for _ in range(lenB0)] for _ in range(lenA)]
+            for i in range(lenA):
+                for j in range(lenB0):
+                    for k in range(lenB):
+                        C[i][j] += A[i][k] * B[k][j]
+        else:  # B is vector
+            C = [0 for _ in range(lenB)]
+            for i in range(lenA):
+                for k in range(lenB):
+                    C[i] += A[i][k] * B[k]
+    return C
+
+
+def matrix_power(A, n, use_numpy=NUMPY):
+    """Multiply matrix A n times.
+
+    :param A: input square matrix.
+    :param n: power.
+    :return B: resulted matrix.
+    """
+    if use_numpy:
+        B = np.linalg.matrix_power(A, n)
+    else:
+        B = copy.deepcopy(A)
+        for i in range(n - 1):
+            B = dot(A, B, use_numpy=use_numpy)
+    return B
+
+
+def _calc_delta(energy=_ENERGY, data_file='Be_delta.dat', use_numpy=NUMPY):
+    skiprows = 2
+    energy_column = 0
+    delta_column = 1
+    error_msg = 'Error! Use energy range from {} to {} eV.'
+    if use_numpy:
+        data = np.loadtxt(data_file, skiprows=skiprows)
+        try:
+            idx_previous = np.where(data[:, energy_column] <= energy)[0][-1]
+            idx_next = np.where(data[:, energy_column] > energy)[0][0]
+        except IndexError:
+            raise Exception(error_msg.format(data[0, energy_column], data[-1, energy_column]))
+
+        idx = idx_previous if abs(data[idx_previous, energy_column] - energy) <= abs(
+            data[idx_next, energy_column] - energy) else idx_next
+        found_energy = data[idx][energy_column]
+        delta = data[idx][delta_column]
+    else:
+        found_energy = None
+        delta = None
+        with open(data_file, 'r') as f:
+            content = f.readlines()
+            energies = []
+            deltas = []
+            for i in range(skiprows, len(content)):
+                energies.append(float(content[i].split()[energy_column]))
+                deltas.append(float(content[i].split()[delta_column]))
+            indices_previous = []
+            indices_next = []
+            try:
+                for i in range(len(energies)):
+                    if energies[i] <= energy:
+                        indices_previous.append(i)
+                    else:
+                        indices_next.append(i)
+                idx_previous = indices_previous[-1]
+                idx_next = indices_next[0]
+            except IndexError:
+                raise Exception(error_msg.format(energies[0], energies[-1]))
+
+            idx = idx_previous if abs(energies[idx_previous] - energy) <= abs(energies[idx_next] - energy) else idx_next
+            found_energy = energies[idx]
+            delta = deltas[idx]
+    return {
+        'energy': found_energy,
+        'delta': delta
+    }
+
+
+def _calc_distance(id1, id2):
+    el_num1 = _find_element_by_id(id1)
+    el_num2 = _find_element_by_id(id2)
+    if not el_num1 or not el_num2:
+        raise Exception('Provided id\'s are not valid!')
+
+    coord1 = _TRANSFOCATOR_CONFIG[el_num1]['offset_cart'] * _DL_cart
+    lens_num1 = _LENS_CONFIG[_TRANSFOCATOR_CONFIG[el_num1]['name']]['lens_number']
+    coord2 = _TRANSFOCATOR_CONFIG[el_num2]['offset_cart'] * _DL_cart
+
+    return coord2 - coord1 - lens_num1 * _DL_lens
+
+
+def _calc_fs(R, delta):
+    return R / (2 * delta)
+
+
+def _calc_T_dl(dl):
+    T_dl = [
+        [1, dl],
+        [0, 1],
+    ]
+    return T_dl
+
+
+def _calc_T_fs(fs):
+    T_fs = [
+        [1, 0],
+        [-1 / fs, 1],
+    ]
+    return T_fs
+
+
+def _find_element_by_id(id, data=_TRANSFOCATOR_CONFIG):
+    element_number = None
+    for i in range(len(data)):
+        if id == data[i]['id']:
+            element_number = i
+            break
+    return element_number
 
 
 if __name__ == '__main__':
-    teta0 = 60e-6  # rad
-    y0 = _P0 * np.tan(teta0)
-    T = calc_T_total([2, 4, 6, 7, 8])
+    y0 = _P0 * math.tan(_TETA0)
+    T = calc_T_total([2, 4, 6])
 
-    v = calc_y_teta(T, y0, teta0)
+    v = calc_y_teta(T, y0, _TETA0)
 
-    P1 = v[0] / np.tan(np.pi - v[1])
+    P1 = v[0] / math.tan(math.pi - v[1])
 
-    f0 = calc_ideal_lens(_LENS_CONFIG['T_1_50']['radius'], 31, calc_delta()['delta'])
+    f0 = calc_ideal_lens(_LENS_CONFIG['T_1_50']['radius'], 22, _calc_delta()['delta'])
     P1_ideal = (1 / (1 / f0 - 1 / _P0))
 
     print()
