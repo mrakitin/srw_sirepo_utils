3e3027d95cdd901c52438c7167c0e58bf8d55210
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-12-03 15:05:19 -0500

    Revert the whole range of magn_field

--- srw_peaks/srw_peaks.py
+++ srw_peaks/srw_peaks.py
@@ -15,8 +15,8 @@ from tqdm import tqdm
 from chx_spectrum import chx_spectrum
 
 # List of By magnetic fields to test:
-# magn_field_range = np.linspace(0.5, 1.0, 51)
-magn_field_range = [0.58]
+magn_field_range = np.linspace(0.5, 1.0, 51)
+# magn_field_range = [0.58]
 columns = ['magn_field', 'energy', 'delta', 'atten']
 scan_plan = pd.DataFrame(columns=columns)
 
